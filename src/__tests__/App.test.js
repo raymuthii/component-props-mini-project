c2972af3a73634ac1abfdc10144f6874138e73fa
Raymond Ruibi Muthii <raymondmuthii@gmail.com>
2024-08-22 23:11:58 +0300

    ...

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,8 +4,8 @@ import App from "../components/App";
 
 test("renders the correct child components", () => {
   const { container } = render(<App />);
-  expect(container.querySelector(".App")).toBeInTheDocument();
-  expect(container.querySelector(".App header")).toBeInTheDocument();
-  expect(container.querySelector(".App aside")).toBeInTheDocument();
-  expect(container.querySelector(".App main")).toBeInTheDocument();
+  expect(container.querySelector(".app")).toBeInTheDocument();
+  expect(container.querySelector(".app > header")).toBeInTheDocument();
+  expect(container.querySelector(".app > aside")).toBeInTheDocument();
+  expect(container.querySelector(".app > main")).toBeInTheDocument();
 });
--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import blogData from "../data/blog";
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Header name={blogData.name} />
       <About image={blogData.image} about={blogData.about} />
       <ArticleList posts={blogData.posts} />
